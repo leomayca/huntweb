bfba1a85cc5cc0a5796da2f49cff5d8df10932ba
Leonardo Mayca <leomt8440@gmail.com>
2020-02-13 13:41:29 -0300

    Finalização: funcionalidade doas acessos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import api from './services/api';
+import Routes from './routes';
 
 import Header from './components/Header';
-import Main from './pages/main';
 
 import "./styles.css";
 
 const App = () => (
   <div className="App">
     <Header />
-    <Main />
+    <Routes />
   </div>
 );
 
